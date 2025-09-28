f912bca19d3307eac4cfcf720cbad0d248c448a3
iihimanshu <iihimanshu.b2@hotmail.com>
2025-09-28 10:06:24 +0530

    cook

--- passport/init.ts
+++ passport/init.ts
@@ -3,11 +3,11 @@ import { googleStrategy } from './googleStrategy';
 import type { CookieOptions } from 'express';
 import { localStrategy } from './customStrategy';
 
-export const cookieObj:CookieOptions = {
-    httpOnly:true,
-    secure:process.env.NODE_ENV === 'development',
-    maxAge:1*24*60*60*1000, // 1 day
-    sameSite:'strict'
+export const cookieObj: CookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+    sameSite: 'lax'
 }
 passport.use(googleStrategy);
 passport.use(localStrategy);
