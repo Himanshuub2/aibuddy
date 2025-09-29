8e7fc0c861d1cf720d997d06581b48d730f3266f
iihimanshu <iihimanshu.b2@hotmail.com>
2025-09-29 14:22:20 +0530

    cook

--- passport/init.ts
+++ passport/init.ts
@@ -7,7 +7,7 @@ export const cookieObj: CookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-    sameSite: 'lax'
+    sameSite: 'none'
 }
 passport.use(googleStrategy);
 passport.use(localStrategy);
