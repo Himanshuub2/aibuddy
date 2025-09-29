b9fe308f373851e895683ee9390b3e7846d52d88
iihimanshu <iihimanshu.b2@hotmail.com>
2025-09-29 17:27:40 +0530

    path

--- passport/init.ts
+++ passport/init.ts
@@ -7,7 +7,9 @@ export const cookieObj: CookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
-    sameSite: 'none'
+    sameSite: 'none',
+    domain: ".himanshuk.in",
+    path: '/'
 }
 passport.use(googleStrategy);
 passport.use(localStrategy);
